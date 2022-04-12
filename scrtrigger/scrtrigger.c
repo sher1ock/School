dfb2e93d0842acd243ca339a6353ce274441c251
sher1ock <uss.voyager.dan@gmail.com>
2022-04-12 12:23:46 -0500

    fixed bug where it was triggering off the wrong edge

--- scrtrigger/scrtrigger.c
+++ scrtrigger/scrtrigger.c
@@ -8,18 +8,14 @@ const uint OUTPIN = 0;
 const uint INPIN = 2;
 const uint DELAYPIN = 26;
 
-int DELAY = 3;
-
-
-
-//typedef void(* irq_handler_t) (void);
+float DELAY = 3;
 
 void gpio_callback(uint gpio, uint32_t events) {
 
     //gpio_put(OUTPIN, 0);
-    busy_wait_ms(DELAY);
+    busy_wait_us(DELAY);
     gpio_put(OUTPIN, 1);
-    busy_wait_ms(1);
+    busy_wait_us(1000);
     gpio_put(OUTPIN, 0);
 }
 
@@ -35,8 +31,9 @@ int main(){
     adc_gpio_init(26);
     // Select ADC input 0 (GPIO26)
     adc_select_input(0);
-    gpio_pull_up(OUTPIN);
-    gpio_set_irq_enabled_with_callback(INPIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_pull_down(OUTPIN);
+    gpio_pull_up(INPIN);
+    gpio_set_irq_enabled_with_callback(INPIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
     while (1){
         //busy_wait_ms(8);
         //gpio_put(OUTPIN, 0);
@@ -44,7 +41,7 @@ int main(){
         const float conversion_factor = 3.3f / (1 << 12);
         uint16_t result = adc_read();
         //printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
-        DELAY = (result*conversion_factor/3.3)*8;
+        DELAY = (result*conversion_factor/3.3)*8000;
         gpio_put(OUTPIN, 0);
 
     }
