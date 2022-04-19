560864df88b568307a36ea95eeccb64e01e4dc33
sher1ock <uss.voyager.dan@gmail.com>
2022-04-19 11:48:24 -0500

    final version of the light sensor.

--- neopixels/neopixels.c
+++ neopixels/neopixels.c
@@ -10,6 +10,8 @@
 
 int light = 1.2;
 #define WS2812_PIN 4
+#define repeat(x) for(int i = x; i--;)
+
 
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -20,6 +22,42 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
             ((uint32_t) (r) << 8) |
             ((uint32_t) (g) << 16) |
             (uint32_t) (b);
+}
+
+
+
+void bootanimation(){
+    sleep_ms(500);
+    int a=0;
+    int b=0;
+    int x=0;
+    repeat(12){
+        put_pixel(urgb_u32(0,10,0));
+
+    }
+    sleep_ms(100);
+    while (a<13){
+        repeat(x){
+            put_pixel(urgb_u32(10,0,0));
+            //x++;
+
+        }
+        a++;
+        sleep_ms(100);
+        x++;
+
+    }
+    x=0;
+    while (b<13){
+        repeat(x){
+            put_pixel(urgb_u32(0,0,10));
+            //x++;
+
+        }
+        b++;
+        sleep_ms(100);
+        x++;
+    }
 }
 
 
@@ -42,8 +80,8 @@ int main() {
     }
 
 
-    int countred=0;
-    int countblue=0;
+    bootanimation();
+    sleep_ms(1000);
     int x = 0;
     int z = 0;
     while (1){
@@ -56,14 +94,12 @@ int main() {
         uint16_t result = adc_read();
         //printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
         z = (result*conversion_factor/3.3)*12;
-        countred = 0;
-        countblue = 0;
+
         
         if (z>0){    
             while (x<z+1){
                 x++;
                 put_pixel(urgb_u32(0, 0, 5));
-                countblue ++;
 
             }
         }
@@ -71,7 +107,6 @@ int main() {
         while (z<12){
             z++;
             put_pixel(urgb_u32(20,0,0));
-            countred++;
         }
         
            
