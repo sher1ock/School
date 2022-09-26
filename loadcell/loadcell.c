dfa5b593779b5421fd8245cb15b3fbad0adc0a7c
sher1ock <uss.voyager.dan@gmail.com>
2022-09-26 12:10:37 -0500

    just cleaning up git

--- loadcell/loadcell.c
+++ loadcell/loadcell.c
@@ -1,196 +1,52 @@
+
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include "pico/stdlib.h"
-#include "hardware/i2c.h"
-#include "pico/binary_info.h"
-#include "ws2812.pio.h"
-#include "hardware/pio.h"
-#include "hardware/clocks.h"
-#include "hardware/adc.h"
-#include "hardware/gpio.h"
+#include "pico/stdio.h"
+#include "hx711.h"
+#include "hx711_noblock.pio.h"
 
+int main(void) {
 
-#define IS_RGBW true
-#define NUM_PIXELS 150
-
-#ifdef PICO_DEFAULT_WS2812_PIN
-#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
-#else
-// default to pin 2 if the board doesn't have a default WS2812 pin defined
-#define WS2812_PIN 0
-#endif
-
-static inline void put_pixel(uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
-}
-
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
-}
-
-const int LCD_CLEARDISPLAY = 0x01;
-const int LCD_RETURNHOME = 0x02;
-const int LCD_ENTRYMODESET = 0x04;
-const int LCD_DISPLAYCONTROL = 0x08;
-const int LCD_CURSORSHIFT = 0x10;
-const int LCD_FUNCTIONSET = 0x20;
-const int LCD_SETCGRAMADDR = 0x40;
-const int LCD_SETDDRAMADDR = 0x80;
-
-// flags for display entry mode
-const int LCD_ENTRYSHIFTINCREMENT = 0x01;
-const int LCD_ENTRYLEFT = 0x02;
-
-// flags for display and cursor control
-const int LCD_BLINKON = 0x01;
-const int LCD_CURSORON = 0x02;
-const int LCD_DISPLAYON = 0x04;
-
-// flags for display and cursor shift
-const int LCD_MOVERIGHT = 0x04;
-const int LCD_DISPLAYMOVE = 0x08;
-
-// flags for function set
-const int LCD_5x10DOTS = 0x04;
-const int LCD_2LINE = 0x08;
-const int LCD_8BITMODE = 0x10;
-
-// flag for backlight control
-const int LCD_BACKLIGHT = 0x08;
-
-const int LCD_ENABLE_BIT = 0x04;
-
-// By default these LCD display drivers are on bus address 0x27
-static int addr = 0x27;
-
-// Modes for lcd_send_byte
-#define LCD_CHARACTER  1
-#define LCD_COMMAND    0
-
-#define MAX_LINES      2
-#define MAX_CHARS      16
-
-/* Quick helper function for single byte transfers */
-void i2c_write_byte(uint8_t val) {
-#ifdef i2c_default
-    i2c_write_blocking(i2c_default, addr, &val, 1, false);
-#endif
-}
-
-void lcd_toggle_enable(uint8_t val) {
-    // Toggle enable pin on LCD display
-    // We cannot do this too quickly or things don't work
-#define DELAY_US 600
-    sleep_us(DELAY_US);
-    i2c_write_byte(val | LCD_ENABLE_BIT);
-    sleep_us(DELAY_US);
-    i2c_write_byte(val & ~LCD_ENABLE_BIT);
-    sleep_us(DELAY_US);
-}
-
-// The display is sent a byte as two separate nibble transfers
-void lcd_send_byte(uint8_t val, int mode) {
-    uint8_t high = mode | (val & 0xF0) | LCD_BACKLIGHT;
-    uint8_t low = mode | ((val << 4) & 0xF0) | LCD_BACKLIGHT;
-
-    i2c_write_byte(high);
-    lcd_toggle_enable(high);
-    i2c_write_byte(low);
-    lcd_toggle_enable(low);
-}
-
-void lcd_clear(void) {
-    lcd_send_byte(LCD_CLEARDISPLAY, LCD_COMMAND);
-}
-
-// go to location on LCD
-void lcd_set_cursor(int line, int position) {
-    int val = (line == 0) ? 0x80 + position : 0xC0 + position;
-    lcd_send_byte(val, LCD_COMMAND);
-}
-
-static void inline lcd_char(char val) {
-    lcd_send_byte(val, LCD_CHARACTER);
-}
-
-void lcd_string(const char *s) {
-    while (*s) {
-        lcd_char(*s++);
-    }
-}
-
-void lcd_init() {
-    lcd_send_byte(0x03, LCD_COMMAND);
-    lcd_send_byte(0x03, LCD_COMMAND);
-    lcd_send_byte(0x03, LCD_COMMAND);
-    lcd_send_byte(0x02, LCD_COMMAND);
-
-    lcd_send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND);
-    lcd_send_byte(LCD_FUNCTIONSET | LCD_2LINE, LCD_COMMAND);
-    lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
-    lcd_clear();
-}
-
-int main() {
-
-    // LED setup
     stdio_init_all();
-    PIO pio = pio0;
-    int sm = 0;
-    uint offset = pio_add_program(pio, &ws2812_program);
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    //LCD setup
-    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    // SET THESE TO THE GPIO PINS CONNECTED TO THE
+    // HX711's CLOCK AND DATA PINS
+    // PINOUT REFERENCE: https://learn.adafruit.com/assets/99339
+    const uint clkPin = 14; // GP14, PAD19
+    const uint datPin = 15; // GP15, PAD20
 
-    lcd_init();
-    // adc setup:
-    adc_init();
+    hx711_t hx;
 
-    // Make sure GPIO is high-impedance, no pullups etc
-    adc_gpio_init(26);
-    // Select ADC input 0 (GPIO26)
-    adc_select_input(0);
+    //1. init the hx711 struct
+    hx711_init(
+        &hx,
+        clkPin,
+        datPin,
+        pio0,
+        &hx711_noblock_program,
+        &hx711_noblock_program_init);
 
-    //char *message[] =
-    //        {
-                    //"RP2040 by", "Raspberry Pi",
-                    // "A brand new", "microcontroller",
-                    // "Twin core M0", "Full C SDK",
-                    // "More power in", "your product",
-                    // "More beans", "than Heinz!"
-    //        };
+    //2. power up the hx711
+    hx711_set_power(&hx, hx711_pwr_up);
 
-    while (1) {
-        float result = adc_read();
-        double output = 2000 * 1/4095;
+    //3. [OPTIONAL] set gain and save it to the HX711
+    //chip by powering down then back up
+    hx711_set_gain(&hx, hx711_gain_128);
+    hx711_set_power(&hx, hx711_pwr_down);
+    hx711_wait_power_down();
+    hx711_set_power(&hx, hx711_pwr_up);
 
-        char display = "output is %s", output;
+    //4. pause to allow the readings to settle based on the
+    //sample rate of the chip
+    hx711_wait_settle(hx711_rate_80);
 
-        lcd_string(display);
-        sleep_ms(2000);
-        lcd_clear();
+    //at this point, the hx711 can reliably produce values
+    //with hx711_get_value or hx711_get_value_timeout
 
-
-        //  for (int m = 0; m < sizeof(message) / sizeof(message[0]); m += MAX_LINES) {
-        //      for (int line = 0; line < MAX_LINES; line++) {
-        //         lcd_set_cursor(line, (MAX_CHARS / 2) - strlen(message[m + line]) / 2);
-        //         lcd_string(message[m + line]);
-        //     }
-        //     sleep_ms(2000);
-        //     lcd_clear();
-        //}
+    for(;;) {
+        printf("%li\n", hx711_get_value(&hx));
     }
 
-    return 0;
+    return EXIT_SUCCESS;
+
 }
