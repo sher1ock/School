742bb42535885d4e5a6f8735842d10e8c3fae42e
sher1ock <uss.voyager.dan@gmail.com>
2022-09-26 12:06:37 -0500

    actually outputting a reasonable number now.

--- loadcellfixed/loadcellfixed.c
+++ loadcellfixed/loadcellfixed.c
@@ -5,12 +5,119 @@
 #include "hx711-pico-c/include/hx711.h"
 #include "hx711-pico-c/include/hx711_noblock.pio.h"
 #include "ws2812.pio.h"
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
 
 
 #define IS_RGBW false
 #define NUM_PIXELS 12
 #define brightness 50
 
+#define repeat(x) for(int i = x; i--;)
+
+// commands
+const int LCD_CLEARDISPLAY = 0x01;
+const int LCD_RETURNHOME = 0x02;
+const int LCD_ENTRYMODESET = 0x04;
+const int LCD_DISPLAYCONTROL = 0x08;
+const int LCD_CURSORSHIFT = 0x10;
+const int LCD_FUNCTIONSET = 0x20;
+const int LCD_SETCGRAMADDR = 0x40;
+const int LCD_SETDDRAMADDR = 0x80;
+
+// flags for display entry mode
+const int LCD_ENTRYSHIFTINCREMENT = 0x01;
+const int LCD_ENTRYLEFT = 0x02;
+
+// flags for display and cursor control
+const int LCD_BLINKON = 0x01;
+const int LCD_CURSORON = 0x02;
+const int LCD_DISPLAYON = 0x04;
+
+// flags for display and cursor shift
+const int LCD_MOVERIGHT = 0x04;
+const int LCD_DISPLAYMOVE = 0x08;
+
+// flags for function set
+const int LCD_5x10DOTS = 0x04;
+const int LCD_2LINE = 0x08;
+const int LCD_8BITMODE = 0x10;
+
+// flag for backlight control
+const int LCD_BACKLIGHT = 0x08;
+
+const int LCD_ENABLE_BIT = 0x04;
+
+// By default these LCD display drivers are on bus address 0x27
+static int addr = 0x27;
+
+// Modes for lcd_send_byte
+#define LCD_CHARACTER  1
+#define LCD_COMMAND    0
+
+#define MAX_LINES      2
+#define MAX_CHARS      16
+
+/* Quick helper function for single byte transfers */
+void i2c_write_byte(uint8_t val) {
+#ifdef i2c_default
+    i2c_write_blocking(i2c_default, addr, &val, 1, false);
+#endif
+}
+
+void lcd_toggle_enable(uint8_t val) {
+    // Toggle enable pin on LCD display
+    // We cannot do this too quickly or things don't work
+#define DELAY_US 600
+    sleep_us(DELAY_US);
+    i2c_write_byte(val | LCD_ENABLE_BIT);
+    sleep_us(DELAY_US);
+    i2c_write_byte(val & ~LCD_ENABLE_BIT);
+    sleep_us(DELAY_US);
+}
+
+// The display is sent a byte as two separate nibble transfers
+void lcd_send_byte(uint8_t val, int mode) {
+    uint8_t high = mode | (val & 0xF0) | LCD_BACKLIGHT;
+    uint8_t low = mode | ((val << 4) & 0xF0) | LCD_BACKLIGHT;
+
+    i2c_write_byte(high);
+    lcd_toggle_enable(high);
+    i2c_write_byte(low);
+    lcd_toggle_enable(low);
+}
+
+void lcd_clear(void) {
+    lcd_send_byte(LCD_CLEARDISPLAY, LCD_COMMAND);
+}
+
+// go to location on LCD
+void lcd_set_cursor(int line, int position) {
+    int val = (line == 0) ? 0x80 + position : 0xC0 + position;
+    lcd_send_byte(val, LCD_COMMAND);
+}
+
+static void inline lcd_char(char val) {
+    lcd_send_byte(val, LCD_CHARACTER);
+}
+
+void lcd_string(const char *s) {
+    while (*s) {
+        lcd_char(*s++);
+    }
+}
+
+void lcd_init() {
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x02, LCD_COMMAND);
+
+    lcd_send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND);
+    lcd_send_byte(LCD_FUNCTIONSET | LCD_2LINE, LCD_COMMAND);
+    lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
+    lcd_clear();
+}
 uint16_t adcresult = 1;
 
 int light = 1.2;
@@ -40,7 +147,7 @@ void bootanimation(){
         put_pixel(urgb_u32(0,10,0));
 
     }
-    sleep_ms(100);
+    sleep_ms(50);
     while (a<13){
         repeat(x){
             put_pixel(urgb_u32(10,0,0));
@@ -48,7 +155,7 @@ void bootanimation(){
 
         }
         a++;
-        sleep_ms(100);
+        sleep_ms(50);
         x++;
 
     }
@@ -65,11 +172,30 @@ void bootanimation(){
     }
 }
 
+int movingAvg(int *ptrArrNumbers, long *ptrSum, int pos, int len, int nextNum)
+{
+  //Subtract the oldest number from the prev sum, add the new number
+  *ptrSum = *ptrSum - ptrArrNumbers[pos] + nextNum;
+  //Assign the nextNum to the position in the array
+  ptrArrNumbers[pos] = nextNum;
+  //return the average
+  return *ptrSum / len;
+}
 
 int main(void) {
 
     stdio_init_all();
 
+     // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    lcd_init();
 
 
     PIO pio = pio0;
@@ -81,7 +207,7 @@ int main(void) {
     for (uint i=0; i<12; i++){
         put_pixel(0);
     }
-
+    
     
     bootanimation();
 
@@ -119,9 +245,30 @@ int main(void) {
 
     //at this point, the hx711 can reliably produce values
     //with hx711_get_value or hx711_get_value_timeout
+    int output = 0;
 
-    for(;;) {
-        printf("%li\n", hx711_get_value(&hx));
+    while(1) {
+
+        
+
+        
+        output = hx711_get_value(&hx);
+
+
+        output = output -2750;
+        if (output < 50){
+            output = 0;
+        }
+        int grams = output /1481;
+
+        char buf[256];
+        sprintf(buf, "%d units,%d grams", output, grams);
+
+
+
+        lcd_string(buf);
+        sleep_ms(400);
+        lcd_clear();
     }
 
     return EXIT_SUCCESS;
